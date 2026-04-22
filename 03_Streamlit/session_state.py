41ac15289982a2fb911c0d2555d4a0e08f7c8219
Ebad1001 <ebadulislamfarooqi@gmail.com>
2026-04-22 23:34:05 +0530

    updated code for streamlit

---
+++ 03_Streamlit/session_state.py
@@ -0,0 +1,17 @@
+# !python -m streamlit run file.py
+
+import streamlit as st
+
+def incr():
+    global temp_counter
+    temp_counter += 1
+    st.session_state.perm_counter += 1
+
+st.button("Increment", on_click=incr)
+
+temp_counter = 0
+st.write(f"TEMP = {temp_counter}")
+
+if "perm_counter" not in st.session_state:
+    st.session_state.perm_counter = 0
+st.write(f"PERM = {st.session_state.perm_counter}")
